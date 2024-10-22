2eb5186a3ee61c72efbbfbf5bfccbd93ca553224
Edgaras <edasedgaras.dir@gmail.com>
2024-10-22 20:34:54 +0300

    added blockchain and block classes

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,6 @@
 import hashlib
 import random
+import time
 
 
 class User:
@@ -7,9 +8,6 @@ class User:
         self.name = name
         self.public_key = public_key
         self.balance = balance
-
-    def __str__(self):
-        return f"I'm {self.name}, key {self.public_key}, {self.balance} balance"
 
 
 class Transaction:
@@ -23,8 +21,68 @@ class Transaction:
         tx_data = f"{self.sender}{self.receiver}{self.amount}"
         return hashlib.sha256(tx_data.encode()).hexdigest()
 
-    def __str__(self):
-        return f"Sender {self.sender}, Receiver {self.receiver}, amount {self.amount}, tx_id {self.tx_id}"
+
+class Block:
+    def __init__(self, prev_hash, transactions, difficulty):
+        self.prev_hash = prev_hash
+        self.timestamp = time.time()
+        self.transactions = transactions
+        self.merkle_root = self.calculate_merkle_root()
+        self.nonce = 0
+        self.difficulty = difficulty
+        self.block_hash = self.mine_block()
+
+    def calculate_merkle_root(self):
+        tx_ids = [tx.tx_id for tx in self.transactions]
+        if len(tx_ids) == 0:
+            return None
+        while len(tx_ids) > 1:
+            if len(tx_ids) % 2 != 0:
+                tx_ids.append(tx_ids[-1])
+            new_level = []
+            for i in range(0, len(tx_ids), 2):
+                combined = tx_ids[i] + tx_ids[i + 1]
+                new_level.append(hashlib.sha256(combined.encode()).hexdigest())
+            tx_ids = new_level
+        return tx_ids[0]
+
+    def calculate_hash(self):
+        block_data = f"{self.prev_hash}{self.timestamp}{self.merkle_root}{self.nonce}{self.difficulty}"
+        return hashlib.sha256(block_data.encode()).hexdigest()
+
+    def mine_block(self):
+        target = "0" * self.difficulty
+        while True:
+            block_hash = self.calculate_hash()
+            if block_hash.startswith(target):
+                return block_hash
+            self.nonce += 1
+
+
+class Blockchain:
+    def __init__(self, difficulty):
+        self.difficulty = difficulty
+        self.chain = [self.create_genesis_block()]
+        self.pending_transactions = []
+
+    def create_genesis_block(self):
+        return Block("0", [], self.difficulty)
+
+    def add_block(self, transactions):
+        prev_hash = self.chain[-1].block_hash
+        new_block = Block(prev_hash, transactions, self.difficulty)
+        self.chain.append(new_block)
+
+    def add_transaction(self, transaction):
+        self.pending_transactions.append(transaction)
+
+    def mine_pending_transactions(self):
+        if len(self.pending_transactions) < 100:
+            return
+        selected_transactions = random.sample(self.pending_transactions, 100)
+        self.add_block(selected_transactions)
+        for tx in selected_transactions:
+            self.pending_transactions.remove(tx)
 
 
 def generate_users(num_users):
@@ -47,10 +105,19 @@ def generate_transactions(users, num_transactions):
     return transactions
 
 
+# Main logic
 if __name__ == "__main__":
-    users = generate_users(4)
-    transactions = generate_transactions(users, 5)
-    for transaction in transactions:
-        print(transaction)
-    for transaction in users:
-        print(transaction)
+    users = generate_users(1000)
+
+    transactions = generate_transactions(users, 10000)
+
+    blockchain = Blockchain(difficulty=4)
+
+    for tx in transactions:
+        blockchain.add_transaction(tx)
+
+    while len(blockchain.pending_transactions) > 0:
+        blockchain.mine_pending_transactions()
+
+    for idx, block in enumerate(blockchain.chain):
+        print(f"Block {idx} - Hash: {block.block_hash}, Previous Hash: {block.prev_hash}")
